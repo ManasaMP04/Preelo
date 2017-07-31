832b06f890fa2ba7ce5187c54ef592a73da2a63c
Manasa MP <manasa.mp@redbus.com>
2017-07-31 17:51:38 +0530

    Show the saved images in document directory
    Show the saved images in document directory

--- Preelo/ImageCell.swift
+++ Preelo/ImageCell.swift
@@ -25,7 +25,7 @@ class ImageCell: UICollectionViewCell {
             
             showImage(msg.thumb_Url)
         } else {
-        
+            
             showImage(msg.image_url)
         }
     }
@@ -36,14 +36,39 @@ class ImageCell: UICollectionViewCell {
     }
     
     fileprivate func showImage(_ image: Any?) {
-    
+        
         if let name = image as? UIImage {
             
             imageView.image   = name
         } else if let name = image as? String {
             
             let imageUrl     = URL(string: name)
-            imageView.sd_setImage(with: imageUrl, placeholderImage: UIImage(named: "Image Placeholder_Fill"))
+            
+            let fileManager = FileManager.default
+            
+            let paths = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent("preelo")
+            var path = name.replacingOccurrences(of: "https://api.preelo.com/api/image/get?token=", with: "")
+            path = path.replacingOccurrences(of: "/", with: "")
+            let imagePath = paths.appendingFormat("/%@", path)
+            
+            if !fileManager.fileExists(atPath: paths) {
+                
+                try! fileManager.createDirectory(atPath: paths, withIntermediateDirectories: true, attributes: nil)
+            }
+            
+            if !fileManager.fileExists(atPath: imagePath) {
+                
+                imageView.sd_setImage(with: imageUrl, placeholderImage: UIImage(named: "Image Placeholder_Fill"), options:.refreshCached, completed: { (image, error, none, url) in
+                    
+                    if let img = self.imageView.image, let imageData = UIImageJPEGRepresentation(img, 1.0) {
+                        
+                        fileManager.createFile(atPath: imagePath as String, contents: imageData, attributes: nil)
+                    }
+                })
+            } else  {
+                
+                imageView.image = UIImage(contentsOfFile: imagePath)
+            }
         }
     }
 }
